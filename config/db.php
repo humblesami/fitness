b67b7517c66b393631ca5164d69da5d99675a666
abihafatima1415-sys <abihafatima1415@gmail.com>
2025-09-28 13:09:27 -0700

    kucch b

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "root";   // your MySQL username
-$pass = "123";       // your MySQL password
+$pass = "";       // your MySQL password
 $db   = "fitness_db";
 
 $conn = new mysqli($host, $user, $pass, $db);
